7976eda1795e99ba9b72da569878cea9ed9e6c2b
Chaitanya Krishna Mullangi <cmullangi@vmware.com>
2019-03-14 21:57:05 +0530

    Creating Example

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todo = [
+    'Task 1',
+    'Task 2',
+    'Task 3'
+  ];
+
+  done = [
+    'Task 4',
+    'Task 5',
+    'Task 6',
+    'Task 7',
+    'Task 8'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+}
